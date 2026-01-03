3faef7e70959bff0d897129f9f62f74acfb5ce95
Jatin Satare <jatinsatare@gmail.com>
2026-01-03 22:36:13 +0530

    added initial logic

---
+++ index.js
@@ -0,0 +1,5 @@
+const search = document.getElementById("searchInput")
+const searchBtn = document.getElementById("searchBtn")
+const displayMovies = document.getElementById("displayMovies")
+const wishListBtn = document.getElementById("wishlistBtn")
+let imdbIdList = []
